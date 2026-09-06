e7c91934c77d2e5b5d4877e656fca3d7af392f73
piyush-9211 <rt218878@gmail.com>
2026-09-06 19:19:27 +0530

    Add Week 6 Category B problems

---
+++ src/main/java/week6/CategoryB/AccessChecker.java
@@ -0,0 +1,68 @@
+public class AccessChecker {
+
+    static String classifyAccess(String fieldModifier, String accessorContext) {
+        if (accessorContext.equals("SAME_CLASS")) {
+            return "ALLOWED";
+        }
+
+        if (accessorContext.equals("SAME_PACKAGE")) {
+            if (fieldModifier.equals("private")) {
+                return "DENIED";
+            }
+            return "ALLOWED";
+        }
+
+        if (accessorContext.equals("DIFFERENT_PACKAGE")) {
+            if (fieldModifier.equals("public")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        if (accessorContext.equals("SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE")) {
+            if (fieldModifier.equals("protected") || fieldModifier.equals("public")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        if (accessorContext.equals("SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE")) {
+            if (fieldModifier.equals("public")) {
+                return "ALLOWED";
+            }
+            return "DENIED";
+        }
+
+        return "DENIED";
+    }
+
+    static String summarizeBatch(String[][] attempts) {
+        int allowed = 0;
+        int denied = 0;
+
+        for (String[] attempt : attempts) {
+            if (classifyAccess(attempt[0], attempt[1]).equals("ALLOWED")) {
+                allowed++;
+            } else {
+                denied++;
+            }
+        }
+
+        return "Allowed: " + allowed + " | Denied: " + denied;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(classifyAccess("private", "SAME_CLASS"));
+        System.out.println(classifyAccess("protected", "DIFFERENT_PACKAGE"));
+        System.out.println(classifyAccess("protected", "SUBCLASS_DIFFERENT_PACKAGE_OWN_TYPE"));
+        System.out.println(classifyAccess("protected", "SUBCLASS_DIFFERENT_PACKAGE_PARENT_TYPE"));
+
+        String[][] attempts = {
+            {"default", "SAME_PACKAGE"},
+            {"default", "DIFFERENT_PACKAGE"},
+            {"public", "DIFFERENT_PACKAGE"}
+        };
+
+        System.out.println(summarizeBatch(attempts));
+    }
+}
